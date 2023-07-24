87e992cc37ffc752c37858da37b94098bf4a1f26
David Jocabs <david@cydeo.com>
2023-07-24 19:48:00 +0300

    1st version of game completed

---
+++ src/Day1Code.java
@@ -0,0 +1,5 @@
+public class Day1Code {
+
+    // Some code for my game
+
+}
---
+++ src/Day2Code.java
@@ -0,0 +1,8 @@
+public class Day2Code {
+    public static void main(String[] args) {
+
+        System.out.println("Day2 code of my game, I created some avatars today");
+
+    }
+
+}
